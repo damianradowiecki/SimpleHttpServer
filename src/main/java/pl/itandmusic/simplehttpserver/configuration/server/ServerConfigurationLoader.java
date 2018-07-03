e5185c79d49922e9e5007df8fee094f63300a497
damian@damian-Lenovo-G780 <damianradowiecki@gmail.com>
2018-07-03 06:11:37 +0200

    ServerConfigurationLoader refactoring

--- src/main/java/pl/itandmusic/simplehttpserver/configuration/server/ServerConfigurationLoader.java
+++ src/main/java/pl/itandmusic/simplehttpserver/configuration/server/ServerConfigurationLoader.java
@@ -3,6 +3,9 @@ package pl.itandmusic.simplehttpserver.configuration.server;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -14,23 +17,45 @@ import pl.itandmusic.simplehttpserver.logger.Logger;
 public class ServerConfigurationLoader {
 
 	private static final Logger logger = Logger.getLogger(ServerConfigurationLoader.class);
-	
+	private static ServerConfig serverConfig;
+
 	public static void load() throws JAXBException, FileNotFoundException {
-		
+
 		logger.info("Server configuration loading.");
-		
-		File serverXml = new File("/home/damian/eclipse-workspace/Other/SimpleHttpServer/config/server.xml");
-		if (serverXml.exists()) {
-			JAXBContext context = JAXBContext.newInstance(ServerConfig.class);
-			Unmarshaller um = context.createUnmarshaller();
-			ServerConfig serverConfig = (ServerConfig) um.unmarshal(new FileReader(serverXml));
-			Configuration.port = serverConfig.getPort();
-			Configuration.appsDirectory = serverConfig.getAppsDirectory();
+
+		Path serverXmlFilePath = resolveServerXmlFilePath();
+
+		if (Files.exists(serverXmlFilePath)) {
+			if(tryToLoadServerConfig(serverXmlFilePath.toFile())) {
+				Configuration.port = serverConfig.getPort();
+				Configuration.appsDirectory = serverConfig.getAppsDirectory();
+			}
 		} else {
 			throw new FileNotFoundException("Cannot find server.xml file");
 		}
-		
+
 		logger.info("Server configuration loaded.");
-		
+
+	}
+
+	private static Path resolveServerXmlFilePath() {
+		Path rootPath = Paths.get("");
+		Path serverXmlFilePath = rootPath.resolve("config").resolve("server.xml");
+		return serverXmlFilePath;
+	}
+
+	private static boolean tryToLoadServerConfig(File serverConfigFile) {
+		try {
+			JAXBContext context = JAXBContext.newInstance(ServerConfig.class);
+			Unmarshaller um;
+
+			um = context.createUnmarshaller();
+			serverConfig = (ServerConfig) um.unmarshal(new FileReader(serverConfigFile));
+			return true;
+		} catch (JAXBException | FileNotFoundException e) {
+			logger.info("Couldn't read server.xml file. Server can not run.");
+			logger.info("Exception message: " + e.getMessage());
+			return false;
+		}
 	}
 }
